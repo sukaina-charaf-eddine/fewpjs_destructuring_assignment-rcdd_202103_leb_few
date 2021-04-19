45f51a1235d65b06a877fbf65358beaff113726a
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-19 11:28:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo,neigh,baa,oink,cluck]=farmAnimals.split(' ');
+const [bessie,,dolly,babe,little]=farmAnimals.split(' ');
+const [blackAndWhite,,black,pink,] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet]= colors;
+const [r, o, y, g, b,,v]= colors;
+const [,,,,,indg,]= colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,7 +15,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-
+const {muppetName,color,song,job,partner} = muppet;
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -24,6 +30,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const {nestedJob,nestedPartner} = nestedMuppet;
+const{song2,song4} =nestedMuppet.album.theMuppetMovie;
 
 // Strings
 
@@ -39,7 +47,7 @@ const nestedMuppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
