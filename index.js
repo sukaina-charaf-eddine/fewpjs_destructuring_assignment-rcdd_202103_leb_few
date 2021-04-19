49bbd180925b8696bfba618d7b70c31d45b8f749
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-19 11:26:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
-const [bessie, ,dolly, babe, little] = farmAnimals.split('');
-const [blackAndWhite, ,black, pink, ,] = farmAnimals.split('');
-
-
+const [moo,neigh,baa,oink,cluck]=farmAnimals.split(' ');
+const [bessie,,dolly,babe,little]=farmAnimals.split(' ');
+const [blackAndWhite,,black,pink,] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-const [r, o, y, g, b, i, v]=colors;
-const [ red, orange, yellow, green, blue, indg, violet]= colors;
-
+const [red, orange, yellow, green, blue, indigo, violet]= colors;
+[r, o, y, g, b,,v]= colors;
+const [,,,,,indg,]= colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -17,8 +15,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-const { muppetName, color, song, job, partner } = muppet;
-
+const {muppetName,color,song,job,partner} = muppet;
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -33,8 +30,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-const { song2, song4}= nestedMuppet.album.theMuppetMovie;
-const { nestedJob, nestedPartner} =nestedMuppet;
+
+const{song2,song4} =nestedMuppet.album.theMuppetMovie;
 
 // Strings
 
@@ -50,7 +47,7 @@ const { nestedJob, nestedPartner} =nestedMuppet;
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
 // Objects
 
